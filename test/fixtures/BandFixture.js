831e7e4fda4cc393aad3bfedcac09f6c92744178
Matthew Vilhauer <matthewvilhauer@gmail.com>
2016-04-16 20:29:58 -0700

    Add Band Model with simple validation

---
+++ test/fixtures/BandFixture.js
@@ -0,0 +1,7 @@
+"use strict";
+
+/**
+ * Fixtures for Band
+ */
+
+module.exports = {};
---
+++ test/unit/models/Band.test.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const assert = require('chai').assert;
+const Model = require('../../../api/models/Band');
+const Fixture = require('../../fixtures/BandFixture');
+
+describe('models:Band', () => {
+  it('Should be tested', () => {
+    assert(false);
+  });
+});
